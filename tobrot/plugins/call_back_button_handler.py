c9a21c66eb44c57b372db1498ea2d86d783d2101
artemiszuk <68847756+artemiszuk@users.noreply.github.com>
2021-01-29 10:58:54 +0530

    Update call_back_button_handler.py

--- tobrot/plugins/call_back_button_handler.py
+++ tobrot/plugins/call_back_button_handler.py
@@ -48,23 +48,3 @@ async def button(bot, update: CallbackQuery):
                     await i_m_s_e_g.edit_text(f"Leech Cancelled by <a href='tg://user?id={update.from_user.id}'>{update.from_user.first_name}</a>")
                 except Exception as e:
                     await i_m_s_e_g.edit_text("<i>FAILED</i>\n\n" + str(e) + "\n#error")
-        elif cb_data == "fuckingdo":
-                LOGGER.info(g_d_list)
-                g_list = os.listdir()
-                LOGGER.info(g_list)
-                g_del_list = list(set(g_list)-set(g_d_list))
-                LOGGER.info(g_del_list)
-                if len(g_del_list) != 0:
-                    for f in g_del_list:
-                        if os.path.isfile(f):
-                            os.remove(f)
-                        else:
-                            shutil.rmtree(f)
-                    await update.message.edit_text(f"Deleted {len(g_del_list)} objects 😬")
-                else:
-                    await update.message.edit_text("Nothing to clear 🙄")
-            else:
-                await update.message.edit_text("You are not allowed to do that 🤭")
-        elif cb_data == "fuckoff":
-            await update.message.edit_text("Okay! fine 🤬")
-				
